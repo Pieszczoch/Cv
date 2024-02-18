f38099daadf25209354b12115d0500faf859df7c
Sebastian Kopacki <sebastian.kopacki5@gmail.com>
2024-02-18 23:07:46 +0100

    add navbar

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -4,7 +4,7 @@ import Navbar from './Navbar';
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="m-0 min-h-[100dvh] flex-col bg-[#EEE5E5] p-0">
+    <div className="m-0 min-h-[100dvh] flex-col bg-[#E5E7E6] p-0">
       <Navbar />
       <main className="grow">{children}</main>
     </div>
