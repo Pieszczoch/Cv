dbb9919ebcd50ccfceba35a8c07829483da02357
Sebastian Kopacki <sebastian.kopacki5@gmail.com>
2024-02-18 23:22:16 +0100

    add footer, add little fixes

---
+++ src/components/layouts/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer id="footer" tabIndex={-1} className="z-50 w-screen bg-[#1E1C1B]">
+      <nav className="mx-auto flex h-16 max-w-[1440px] items-center justify-between">
+        <h1 className="text-neutral-100">Webstie by Pieszczoch</h1>
+      </nav>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -1,12 +1,14 @@
 import { ReactNode } from 'react';
 
 import Navbar from './Navbar';
+import Footer from './Footer';
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="m-0 min-h-[100dvh] flex-col bg-[#E5E7E6] p-0">
+    <div className="flex m-0 min-h-[100dvh] flex-col bg-[#E5E7E6] p-0">
       <Navbar />
-      <main className="grow">{children}</main>
+      <main className="grow w-full flex-1 mx-auto">{children}</main>
+      <Footer />
     </div>
   );
 };
